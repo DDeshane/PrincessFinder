0d414db6ff6dfeee181e8f5d15eb8a40946c3bf0
Dinah Deshane <dinahdeshane@gmail.com>
2018-12-20 22:56:46 -0500

    Week 14 Homework

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+var friends = [
+    {
+        Name:"Princess Elsa",
+        Photo:"https://www.google.com/imgres?imgurl=https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Elsa_from_Disney%2527s_Frozen.png/220px-Elsa_from_Disney%2527s_Frozen.png&imgrefurl=https://en.wikipedia.org/wiki/Elsa_(Frozen)&h=437&w=220&tbnid=r9fGhHA5pWG9CM:&q=Elsa+(Disney+character)&tbnh=186&tbnw=93&usg=AI4_-kQsKC2b_vDM2ixLo6RsPz9U1AuqfA&vet=12ahUKEwjn5oWusq_fAhUuT98KHaxHAyIQ_B0wHnoECAIQBg..i&docid=UXhsNFn2klj34M&itg=1&sa=X&ved=2ahUKEwjn5oWusq_fAhUuT98KHaxHAyIQ_B0wHnoECAIQBg.jpg",
+        Scores:[
+        "5",
+        "4",
+        "4",
+        "3",
+        "2",
+        "1",
+        "1",
+        "5",
+        "3",
+        "3"
+        ]
+    },
+
+    {
+        Name:"Pocahontas",
+        Photo:"https://www.google.com/imgres?imgurl=https://i.pinimg.com/originals/cf/bd/e1/cfbde10c77b6f5879cb2372f035d0f03.jpg&imgrefurl=https://www.pinterest.com/pin/566890671822585983/&h=1747&w=1000&tbnid=wZv0rSndVwdK7M:&q=Pocahontas+(character)&tbnh=160&tbnw=91&usg=AI4_-kQgVQSKThvSfdJxZybKzoHn31XyrA&vet=12ahUKEwiJ0Me3r6_fAhWuY98KHWIqDEsQ_B0wD3oECAkQFA..i&docid=x5AKeZv8a_c84M&itg=1&sa=X&ved=2ahUKEwiJ0Me3r6_fAhWuY98KHWIqDEsQ_B0wD3oECAkQFA#h=1747&imgdii=9W_EiQxP3LXgHM:&tbnh=160&tbnw=91&vet=12ahUKEwiJ0Me3r6_fAhWuY98KHWIqDEsQ_B0wD3oECAkQFA..i&w=1000.jpg",
+        Scores:[
+        "2",
+        "5",
+        "3",
+        "2",
+        "5",
+        "4",
+        "3",
+        "1",
+        "1",
+        "2"
+        ]
+    },
+
+    {
+        Name: "Ariel",
+        Photo: "https://www.google.com/imgres?imgurl=https://upload.wikimedia.org/wikipedia/en/7/77/Ariel_disney.png&imgrefurl=https://en.wikipedia.org/wiki/Ariel_(The_Little_Mermaid)&h=461&w=216&tbnid=ncBSh2CkfUbTpM:&q=Ariel+(The+Little+Mermaid)&tbnh=186&tbnw=87&usg=AI4_-kQDd_f-aEhI6JFN5appZSXtCRraBw&vet=12ahUKEwjWoYb8ra_fAhVJmuAKHZpPCSsQ_B0wFXoECAIQCQ..i&docid=tMm6HQSpqS2iUM&itg=1&sa=X&ved=2ahUKEwjWoYb8ra_fAhVJmuAKHZpPCSsQ_B0wFXoECAIQCQ.jpg",
+        Scores:[
+        "3",
+        "4",
+        "5",
+        "2",
+        "5",
+        "2",
+        "5",
+        "1",
+        "1",
+        "5",
+        ]
+    },
+
+    {
+        Name: "Bell",
+        Photo:"https://www.google.com/search?q=Belle+(Beauty+and+the+Beast)&sa=X&stick=H4sIAAAAAAAAAONgFmJQ4tTP1TdINqnMLtAS8i7wKXfJLM5LrQwoysxLTi0ufsSozy3w8sc9YSm1SWtOXmNU4MKiSImrad-KQ2wsHIwCDDwAtAyoVVUAAAA&tbm=isch&source=iu&ictx=1&fir=kCP_Cirk2tSeGM%253A%252CyUi4FXpojNGG5M%252C_&usg=AI4_-kQmCPEwFxm0FS8kWXmuYAa5IE0xyg&ved=2ahUKEwjjotWKsK_fAhVHU98KHS8hDX8Q_h0wGHoECAgQDw#imgrc=kCP_Cirk2tSeGM:.jpg",
+        Scores:[
+        "1",
+        "3",
+        "2",
+        "5",
+        "5",
+        "2",
+        "3",
+        "2",
+        "5",
+        "2"
+        ]
+    },
+
+    {
+        Name: "Cinderalla",
+        Photo:"https://www.google.com/imgres?imgurl=https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Cinderella_disney.png/200px-Cinderella_disney.png&imgrefurl=https://en.wikipedia.org/wiki/Cinderella_(Disney_character)&h=301&w=200&tbnid=zqGBIfRI_OVGLM:&q=Cinderella+(Disney+character)&tbnh=160&tbnw=106&usg=AI4_-kQR9pS-31n3ppJtatY7aGLWxiEJEA&vet=12ahUKEwjpre-ssa_fAhUmh-AKHW1GAA0Q_B0wHnoECAUQFA..i&docid=-a9hC329e-uf8M&itg=1&sa=X&ved=2ahUKEwjpre-ssa_fAhUmh-AKHW1GAA0Q_B0wHnoECAUQFA.jpg",
+        Scores:[
+        "2",
+        "4",
+        "2",
+        "3",
+        "5",
+        "2",
+        "5",
+        "2",
+        "1",
+        "5"
+        ]
+    }
+
+];
+    
+ module.exports = friends;   
