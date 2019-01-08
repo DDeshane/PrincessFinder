7790e541eb1c3f30e01797e3cb214ae1de0b533b
Dinah Deshane <dinahdeshane@gmail.com>
2019-01-08 18:15:45 -0500

    change to routes

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
  app.use(bodyParser.text({ type: 'text/html'}))
 
-  require("./app/routing/apiroutes.js")(app);
-  require('./app/routing/htmlroutes.js')(app);
+  require("./app/routing/apiRoutes.js")(app);
+  require('./app/routing/htmlRoutes.js')(app);
 
   app.listen(PORT, function(){
       console.log("App listening on PORT: " + PORT);
